af1266b28e82e4c12199b1327f719c0208735ce1
Pieter Swinkels <swinkels.pieter@yahoo.com>
2019-04-09 21:11:40 +0200

    At the start of chapter 11 Facelift

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask
 from .config import Config
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,6 +14,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 migrate = Migrate(app, db)
 
 from app import routes, models, errors
