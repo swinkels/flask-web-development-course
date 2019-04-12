085c7748e34e5f20a4a7751cd2a92e52e8334fe4
Pieter Swinkels <swinkels.pieter@yahoo.com>
2019-04-12 14:53:28 +0200

    At the start of chapter 12 Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask
 from .config import Config
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -11,6 +12,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ followers = db.Table('followers',
 def load_user(id):
     return User.query.get(int(id))
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
