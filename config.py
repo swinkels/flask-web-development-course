e2406cc24fcf4f160da2779b88bb968aec99f07c
Pieter Swinkels <swinkels.pieter@yahoo.com>
2019-05-05 17:25:24 +0200

    Add changes for Heroku deployment

--- config.py
+++ config.py
@@ -6,17 +6,18 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
+    ADMINS = ['your-email@example.com']
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LANGUAGES = ['en', 'nl']
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    POSTS_PER_PAGE = 25
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
-    LANGUAGES = ['en', 'nl']
-    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-    POSTS_PER_PAGE = 25
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
